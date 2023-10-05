7d3e79db64c4b9e797d0ca3f684185726a3fc0f5
Barusubea <hayate-makishi-k23h@stu.kbc.ac.jp>
2023-10-05 17:42:25 +0900

    Blockcppの追加
    Blockcppを追加しました

--- Tetris/Tetris/AppMain.cpp
+++ Tetris/Tetris/AppMain.cpp
@@ -48,17 +48,51 @@ int WINAPI WinMain(_In_ HINSTANCE hinstance, _In_opt_ HINSTANCE hPrevinstance, _
 	//Dxライブラリの初期化処理
 	if (DxLib_Init() == -1)
 	{
+		//エラー処理
 		return-1;
 
 	}
 
-	//入力待機
-	WaitKey();
+	//フレーム制御機能初期化処理
+	FreamControl_Initialize();
 
+	//シーン制御機能初期化処理
+	SceneManager_Initialize(E_TITLE);
+
+	//入力制御機能初期化処理
+	InputControl_Initialize();
+
+	//描画先画面を裏にする
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//文字サイズを設定
+	SetFontSize(FONT_SIZE);
+
+	//ゲームループ
+	while (ProcessMessge() == 0 && GetExitButton() != TRUE && ErrorCheck() == D_NORMALITY)
+	{
+		//フレーム制御機能更新処理
+		FreamControl_Update();
+
+		//画面の初期化
+		ClearDrawScreen();
+
+		//入力制御機能更新処理
+		InputControl_Update();
+
+		//シーン管理機能更新処理
+		ScenManager_Update();
+
+		//シーン管理機能描画処理
+		SceneManager_Draw();
+
+		//裏画面の内容を表画面に反映
+		ScreenFlip();
+	}
 
 	//Dxライブラリの使用の終了処理
 	DxLib_End();
 
-
+	//ソフトの終了
 	return 0;
 }
